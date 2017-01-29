3196b2caeba08a50cc1c460ddfe69c2f0ca88999
Tina Müller <cpan2@tinita.de>
2017-01-29 01:09:04 +0100

    Less noisy, cut long output from scripts
    Add output summary

--- bin/run-framework-tests-docker.sh
+++ bin/run-framework-tests-docker.sh
@@ -13,14 +13,17 @@ if [[ ! -d out ]]; then
     echo "/yaml-test-suite-data/out does not exist"
     exit 1
 fi
+
+echo "Running tests in docker..."
 for id in [A-Z0-9]*
 #for id in 229Q
 do
-    echo $id
+    echo -n "Running $id"$'\r'
     timeout 3 $framework < $id/in.yaml > out/$id.error 2>&1
     if [[ $? -eq 0 ]]; then
         mv out/$id.error out/$id.ok
     fi
 done
+echo "Done        "
 
 chmod -R go+w out
