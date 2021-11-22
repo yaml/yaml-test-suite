79aa0e5242cb05cabdbb160e3ddebc0e39eea83e
Ingy döt Net <ingy@ingy.net>
2021-11-22 06:37:28 -0800

    Use 4 space indent in perl

--- bin/YAMLTestSuite.pm
+++ bin/YAMLTestSuite.pm
@@ -10,17 +10,17 @@ use YAML::PP;
 my $ypp = YAML::PP->new;
 
 sub new {
-  my $class = shift;
-  my $self = bless {
-    data => undef,
-    id => undef,
-    num => undef,
-    ID => undef,
-    file => 0,
-    skip => 0,
-    make => 0,
-  }, $class;
-  return $self;
+    my $class = shift;
+    my $self = bless {
+        data => undef,
+        id => undef,
+        num => undef,
+        ID => undef,
+        file => 0,
+        skip => 0,
+        make => 0,
+    }, $class;
+    return $self;
 }
 
 sub initial {}
@@ -29,71 +29,71 @@ sub final {}
 sub skip {1}
 
 sub run {
-  my ($self, $files) = @_;
+    my ($self, $files) = @_;
 
-  $self->initial;
+    $self->initial;
 
-  for my $file (@$files) {
-    $self->{file}++;
+    for my $file (@$files) {
+        $self->{file}++;
 
-    ($self->{id} = $file) =~ s{^.*/(.*)\.yaml$}{$1};
+        ($self->{id} = $file) =~ s{^.*/(.*)\.yaml$}{$1};
 
-    my $data = $ypp->load_file($file);
+        my $data = $ypp->load_file($file);
 
-    if ($data->[0]{skip}) {
-      if ($self->skip) {
-        $self->{skip}++;
-        next;
-      }
+        if ($data->[0]{skip}) {
+            if ($self->skip) {
+                $self->{skip}++;
+                next;
+            }
+        }
+
+        my $l = (@$data > 1)
+            ? int(log(@$data - 1) / log(10)) + 2
+            : 2;
+        $self->{cache} = {};
+        delete $self->{num};
+        my $i = 0;
+        my $name = '';
+        for my $test (@$data) {
+            $name = $self->{name} = $test->{name} || $name;
+            $self->{data} = $test;
+            $self->{ID} = $self->{num}
+                ? "$self->{id}-$self->{num}"
+                : $self->{id};
+            $self->make;
+            $self->{make}++;
+            $self->{num} = sprintf "%0${l}d", ++$i;
+        }
+        $self->done;
     }
 
-    my $l = (@$data > 1)
-      ? int(log(@$data - 1) / log(10)) + 2
-      : 2;
-    $self->{cache} = {};
-    delete $self->{num};
-    my $i = 0;
-    my $name = '';
-    for my $test (@$data) {
-      $name = $self->{name} = $test->{name} || $name;
-      $self->{data} = $test;
-      $self->{ID} = $self->{num}
-        ? "$self->{id}-$self->{num}"
-        : $self->{id};
-      $self->make;
-      $self->{make}++;
-      $self->{num} = sprintf "%0${l}d", ++$i;
-    }
-    $self->done;
-  }
-
-  $self->final;
+    $self->final;
 }
 
 sub unescape {
-  my ($self, $text) = @_;
+    my ($self, $text) = @_;
 
-  $text =~ s/␣/ /g;
-  $text =~ s/—*»/\t/g;
-  $text =~ s/←/\r/g;
-  $text =~ s/⇔/x{FEFF}/g;
-  $text =~ s/↵//g;
-  $text =~ s/∎\n\z//;
+    $text =~ s/␣/ /g;
+    $text =~ s/—*»/\t/g;
+    $text =~ s/←/\r/g;
+    $text =~ s/⇔/x{FEFF}/g;
+    $text =~ s/↵//g;
+    $text =~ s/∎\n\z//;
 
-  return $text;
+    return $text;
 }
 
 sub play_url {
-  my ($self, $text) = @_;
+    my ($self, $text) = @_;
 
-  $text = encode_utf8 $self->unescape($text);
+    $text = encode_utf8 $self->unescape($text);
 
-  my $base64 = encode_base64($text);
-  $base64 =~ s{\n}{}g;
-  $base64 =~ s{\+}{-}g;
-  $base64 =~ s{/}{_}g;
+    my $base64 = encode_base64($text);
+    $base64 =~ s{\n}{}g;
+    $base64 =~ s{\+}{-}g;
+    $base64 =~ s{/}{_}g;
 
-  return "https://play.yaml.io/main/parser?input=$base64";
+    return "https://play.yaml.io/main/parser?input=$base64";
 }
 
 1;
--- bin/suite-to-data.pl
+++ bin/suite-to-data.pl
@@ -7,61 +7,61 @@ use base 'YAMLTestSuite';
 use Encode;
 
 my %map = (
-  'name' => '===',
-  'fail' => 'error',
-  'yaml' => 'in.yaml',
-  'tree' => 'test.event',
-  'json' => 'in.json',
-  'dump' => 'out.yaml',
-  'emit' => 'emit.yaml',
-  'toke' => 'lex.token',
+    'name' => '===',
+    'fail' => 'error',
+    'yaml' => 'in.yaml',
+    'tree' => 'test.event',
+    'json' => 'in.json',
+    'dump' => 'out.yaml',
+    'emit' => 'emit.yaml',
+    'toke' => 'lex.token',
 );
 
 main->new->run([@ARGV]);
 
 sub make {
-  my ($self) = @_;
+    my ($self) = @_;
 
-  my ($id, $ID, $num, $data, $cache) =
-    @$self{qw<id ID num data cache>};
+    my ($id, $ID, $num, $data, $cache) =
+        @$self{qw<id ID num data cache>};
 
-  my $dir = "data/$id";
-  mkdir $dir unless -d $dir;
+    my $dir = "data/$id";
+    mkdir $dir unless -d $dir;
 
-  if ($num) {
-    $dir .= "/$num";
-    mkdir $dir or die $dir;
-  }
-
-  for my $k (sort keys %map) {
-    if (exists $data->{$k} and not defined $data->{$k}) {
-      delete $cache->{$k};
-      next;
+    if ($num) {
+        $dir .= "/$num";
+        mkdir $dir or die $dir;
     }
-    $_ = $data->{$k} // $cache->{$k};
-    if (defined $_) {
-      $cache->{$k} = $_;
-      if ($k eq 'name') {
-        $_ .= "\n";
-      }
-      elsif ($k eq 'fail') {
-        $_ = '';
-      }
-      elsif ($k eq 'tree') {
-        s/^\s+//mg;
-        s/\n*\z/\n/;
-      }
 
-      $_ = $self->unescape($_);
+    for my $k (sort keys %map) {
+        if (exists $data->{$k} and not defined $data->{$k}) {
+            delete $cache->{$k};
+            next;
+        }
+        $_ = $data->{$k} // $cache->{$k};
+        if (defined $_) {
+            $cache->{$k} = $_;
+            if ($k eq 'name') {
+                $_ .= "\n";
+            }
+            elsif ($k eq 'fail') {
+                $_ = '';
+            }
+            elsif ($k eq 'tree') {
+                s/^\s+//mg;
+                s/\n*\z/\n/;
+            }
 
-      open my $out, '>', "$dir/$map{$k}" or die;
-      print $out encode_utf8($_);
-      close $out;
+            $_ = $self->unescape($_);
+
+            open my $out, '>', "$dir/$map{$k}" or die;
+            print $out encode_utf8($_);
+            close $out;
+        }
     }
-  }
 }
 
 sub final {
-  my ($self) = @_;
-  warn "Wrote $self->{make} data tests.\n";
+    my ($self) = @_;
+    warn "Wrote $self->{make} data tests.\n";
 }
